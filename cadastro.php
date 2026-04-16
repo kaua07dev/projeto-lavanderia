9d795638b553cdb2d0cbe64d994ffa030732b487
kaua07dev <kcavalcantinoboa@gmail.com>
2026-04-16 16:29:04 -0300

    Criar cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,16 @@
+
+
+<link rel="stylesheet" href="style.css">
+
+<section>
+<h2>Cadastro</h2>
+
+<form action="salvar.php" method="POST">
+<input type="text" name="nome" placeholder="Nome" required>
+<input type="email" name="email" placeholder="Email" required>
+<input type="password" name="senha" placeholder="Senha" required>
+<button>Cadastrar</button>
+</form>
+
+<a href="index.php"><button>Voltar</button></a>
+</section>
